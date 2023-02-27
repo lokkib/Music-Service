7abdf03f7f73ca33451a3edbf518ac00f88b74a2
Sergey <lokkichebrikov@yandex.ru>
2023-02-27 19:24:30 +0300

    edit

--- src/components/Bar/BarContent/BarPlayerBlock/BarPlayerPlayer/PlayerTrackPlay/TrackPlayContain/TrackPlayContain.tsx
+++ src/components/Bar/BarContent/BarPlayerBlock/BarPlayerPlayer/PlayerTrackPlay/TrackPlayContain/TrackPlayContain.tsx
@@ -2,13 +2,13 @@ import { useContext } from 'react';
 import { useSelector } from 'react-redux';
 
 import ThemeContext from '../../../../../../../themes';
-import { TrackPlayAlbum } from './TrackPlayContainComponents/TrackPlayAlbum';
-import { TrackPlayAlbumLink } from './TrackPlayContainComponents/TrackPlayAlbumLink';
-import { TrackPlayAuthor } from './TrackPlayContainComponents/TrackPlayAuthor';
-import { TrackPlayAuthorLink } from './TrackPlayContainComponents/TrackPlayAuthorLink';
-import { TrackPlayContainBlock } from './TrackPlayContainComponents/TrackPlayContainBlock';
-import { TrackPlayContainIcon } from './TrackPlayContainComponents/TrackPlayContainIcon';
-import { TrackPlayImage } from './TrackPlayContainComponents/TrackPlayImage';
+import { TrackPlayAlbum } from './TrackPlayComponents/TrackPlayAlbum';
+import { TrackPlayAlbumLink } from './TrackPlayComponents/TrackPlayAlbumLink';
+import { TrackPlayAuthor } from './TrackPlayComponents/TrackPlayAuthor';
+import { TrackPlayAuthorLink } from './TrackPlayComponents/TrackPlayAuthorLink';
+import { TrackPlayContainBlock } from './TrackPlayComponents/TrackPlayContainBlock';
+import { TrackPlayContainIcon } from './TrackPlayComponents/TrackPlayContainIcon';
+import { TrackPlayImage } from './TrackPlayComponents/TrackPlayImage';
 import { RootState } from '../../../../../../../redux/store';
 
 const TrackPlayContain = () => {
